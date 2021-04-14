e2d7a39ba3258c0b3c808dc7dbafd1595e6c2b2b
Coragg <v_e_c_g@outlook.com>
2021-04-14 19:31:41 -0400

    correcciones en la función azar

--- toque_queda.py
+++ toque_queda.py
@@ -2,23 +2,26 @@
 # Con Sofia Castro, Anibal Muñoz y Victor Camero
 import random
 
-# Funcion de los numeros azar
+# cuatro numeros aleatorios que no se repiten entre si
 def numeros_azar():
-     primer_numero = random.randrange(10)
-     segundo_numero = random.randrange(10)
-     tercer_numero = random.randrange(10)
-     cuarto_numero = random.randrange(10)
-     if primer_numero != segundo_numero and primer_numero != tercer_numero and primer_numero != cuarto_numero and segundo_numero != tercer_numero and segundo_numero != cuarto_numero and tercer_numero != cuarto_numero:
-         print(primer_numero, segundo_numero, tercer_numero, cuarto_numero)
-     else:
-          print("Vuelva a correr el programa")
+    primer = random.randrange(10)
+    segundo_numero = random.randrange(10)
+    tercer_numero = random.randrange(10)
+    cuarto_numero = random.randrange(10)
+    while primer == segundo_numero or primer == tercer_numero or primer == cuarto_numero or segundo_numero == tercer_numero or segundo_numero == cuarto_numero or tercer_numero == cuarto_numero:
+        primer = random.randrange(10)
+        segundo_numero = random.randrange(10)
+        tercer_numero = random.randrange(10)
+        cuarto_numero = random.randrange(10)
+    return primer, segundo_numero, tercer_numero, cuarto_numero
 
-print(numeros_azar())
+
 
 turnos_a_jugar = int(input("Ingrese los turnos a jugar:"))
 for partidas in range(turnos_a_jugar):
     turno = partidas + 1
     intento = input(f"{turno} ingrese los numeros:")
+
 
 
 
