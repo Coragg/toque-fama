b78dd01280d93f00a2c902edd3489f8f8aaaa017
Coragg <v_e_c_g@outlook.com>
2021-04-17 22:31:06 -0400

    no mostrar los numeros aleatorios

--- toque_queda.py
+++ toque_queda.py
@@ -17,14 +17,14 @@ while True:
         tercer_numero = random.randrange(10)
         cuarto_numero = random.randrange(10)
         if primer_numero != segundo_numero and primer_numero != tercer_numero and primer_numero != cuarto_numero and segundo_numero != tercer_numero and segundo_numero != cuarto_numero and tercer_numero != cuarto_numero:
-            print(primer_numero, segundo_numero, tercer_numero, cuarto_numero)
+            numeros = primer_numero, segundo_numero, tercer_numero, cuarto_numero
         else:
             while primer_numero == segundo_numero or primer_numero == tercer_numero or primer_numero == cuarto_numero or segundo_numero == tercer_numero or segundo_numero == cuarto_numero or tercer_numero == cuarto_numero:
                 primer_numero = random.randrange(10)
                 segundo_numero = random.randrange(10)
                 tercer_numero = random.randrange(10)
                 cuarto_numero = random.randrange(10)
-            print(primer_numero, segundo_numero, tercer_numero, cuarto_numero)
+            numeros = primer_numero, segundo_numero, tercer_numero, cuarto_numero
         famas = 0
         toques = 0
         turnos_a_jugar = int(input("Ingrese los turnos a jugar:"))
