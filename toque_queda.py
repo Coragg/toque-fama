9fd3771199de18c4593e4c65de1a289661e20cf0
Indominus22 <82047079+Indominus22@users.noreply.github.com>
2021-04-17 21:58:30 -0400

    toque y fama ahora se puede ganar

--- toque_queda.py
+++ toque_queda.py
@@ -23,7 +23,8 @@ while True:
                 c = random.randrange(10)
                 d = random.randrange(10)
             print(a, b, c, d)
-
+        i=0
+        t=0
         turnos_a_jugar = int(input("Ingrese los turnos a jugar:"))
         for partidas in range(turnos_a_jugar):
             turno = partidas + 1
@@ -31,24 +32,47 @@ while True:
             if len(intento) == 4:
                 print("Se acepta")
                 if(str(a)==intento[0]):
-                    print("F")
+                    i=i+1
                 elif(str(a)==intento[1] or str(a)==intento[2] or str(a)==intento[3]):
-                    print("T")
+                    t=t+1
                 if(str(b)==intento[1]):
-                    print("F")
+                    i=i+1
                 elif(str(b)==intento[0] or str(b)==intento[2] or str(b)==intento[3]):
-                    print("F")
+                    t=t+1
                 if(str(c)==intento[2]):
-                    print("F")
+                    i=i+1
                 elif(str(c)==intento[0] or str(c)==intento[1] or str(c)==intento[3]):
-                    print("T")
+                    t=t+1
                 if(str(d)==intento[3]):
-                    print("F")
+                    i=i+1
                 elif(str(d)==intento[0] or str(d)==intento[1] or str(d)==intento[2]):
-                    print("T")
+                    t=t+1
+                if(i==0):
+                    print("Fama=0")
+                elif(i==1):
+                    print("Fama=1")
+                elif(i==2):
+                    print("Fama=2")
+                elif(i==3):
+                    print("Fama=3")
+                elif(i==4):
+                    print("Fama=4 !!FELICIDADES GANASTE¡¡")
+                    break
+                if(t==0):
+                    print("Toque=0")
+                elif(t==1):
+                    print("Toque=1")
+                elif(t==2):
+                    print("Toque=2")
+                elif(t==3):
+                    print("Toque=3")
+                elif(t==4):
+                    print("Toque=4")
+                i=0
+                t=0
             else:
                 print("No se acepta")
-
+                break
     elif confirmar_jugar.upper() == "N":
         print("Espero que quieras volver a jugar.")
         exit()
